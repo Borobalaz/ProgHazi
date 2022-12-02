13377d648a87f27e3e9cd95d503e6499385f7fbb
Böröczky Balázs <Brczky Balzs@192.168.0.16>
2022-12-02 05:08:38 +0100

    test cases added TODO: documentation

--- GravitySim/src/gravitysim/MassObject.java
+++ GravitySim/src/gravitysim/MassObject.java
@@ -10,6 +10,9 @@ public class MassObject {
 	private int radius;
 	private Color color;
 	
+	/*
+	 * Default constructor
+	 */
 	public MassObject() {
 		mass = 0;
 		radius = 0;
@@ -18,6 +21,14 @@ public class MassObject {
 		wPos = new vec2(position.x, -position.y);
 	}
 	
+	/*
+	 * Constructor
+	 * 
+	 * @param p position of the object
+	 * @param m mass of the object
+	 * @param r radius of the object
+	 * @param c color of the object
+	 */
 	public MassObject(vec2 p, int m, int r, Color c) {
 		mass = m;
 		radius = r;
@@ -26,6 +37,13 @@ public class MassObject {
 		wPos = new vec2(position.x, -position.y);
 	}
 	
+	/*
+	 *  Detects if two objects have collided
+	 *  
+	 *  @param p position of the object that could have collided
+	 *  @param r radius of the object that could have collided
+	 *  @return true if the objects have collided, else false
+	 */
 	public boolean collide(vec2 p, int r) {
 		
 		float dist = wPos.minus(p).length();
@@ -33,21 +51,35 @@ public class MassObject {
 		else return true;
 	}
 	
+	/*
+	 * getters
+	 */
 	public int getMass() {	return mass;}
 	public int getRadius() {	return radius;}
 	public Color getColor()	{	return color;}
 	public vec2 getPos() {	return position;}
 	public vec2 getWPos() {	return wPos;}
+	
+	/*
+	 * setters
+	 */
 	public void setMass(int m)	{	mass = m;}
 	public void setRadius(int r)	{	radius = r;}
 	public void setColor(Color c)	{	color = c;}
 	public void setPos(vec2 p)	{	position = p;}
-	public String toString() {	return "pos: " + position + " mass: " + mass + " radius: " + radius;}
 	public void setWPos(vec2 _wpos) {	wPos = _wpos;}
 	
-	//@Override
-	//public Class<?> getClass()	{	return MassObject.class;}
+	/*
+	 * Converts object data to string
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {	return "pos: " + position + " mass: " + mass + " radius: " + radius;}
 	
+	/*
+	 * Draws a circle in the position of the object 
+	 * 
+	 * @param g the graphic to which the circle will be drawn
+	 */
 	public void draw(Graphics g) {
 		position = new vec2(wPos.x, -wPos.y);
 		g.setColor(color);
--- GravitySim/src/gravitysim/vec2.java
+++ GravitySim/src/gravitysim/vec2.java
@@ -5,39 +5,98 @@ public class vec2 {
 	public float x;
 	public float y;
 	
+	/*
+	 * Constructor
+	 * 
+	 * @param a x coordinate
+	 * @param b y coordinate
+	 */
 	public vec2(float a, float b) {
 		x = a;
 		y = b;
 	}
 	
+	/*
+	 * Vector subtraction 
+	 * 
+	 * @param tmp subtrahend
+	 * @return this - tmp
+	 */
 	public vec2 minus(vec2 tmp) {
 		return new vec2(x - tmp.x, y - tmp.y);
 	} 
+	
+	/*
+	 * Vector addition
+	 * 
+	 * @param tmp addent
+	 * @return this + tmp
+	 */
 	public vec2 plus(vec2 tmp) {
 		return new vec2(x + tmp.x, y + tmp.y);
 	}
+	
+	/*
+	 * Dot product of two vectors
+	 * 
+	 * @param tmp 
+	 * @return Dot product of this and tmp
+	 */
 	public float dot(vec2 tmp) {
 		return (x * tmp.x) + (y * tmp.y);
 	}
+	
+	/*
+	 * 
+	 * @return length of the vector
+	 */
 	public float length() {
 		return (float)Math.sqrt((x * x) + (y * y));
 	}
+	
+	/*
+	 * @return the vector normalized
+	 */
 	public vec2 normalize() {
 
 		return new vec2(x / this.length(), y / this.length());
 	}
+	
+	/*
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
 		return x + "," + y;
 	}
+	
+	/*
+	 * Multipication by a scalar
+	 * 
+	 * @param f scalar
+	 * @return result
+	 */
 	public vec2 multiply(float f) {
 		
 		return new vec2(x*f, y*f);
 	}
+	
+	/*
+	 * Division by scalar
+	 * 
+	 * @param f divisor
+	 * @return result
+	 */
 	public vec2 divide(float f) {
 		
 		return new vec2(x/f, y/f);
 	}
 	
+	/*
+	 * Parsing a string to vec2
+	 * 
+	 * @param string string to be parsed
+	 * @return parsed vec2 type
+	 */
 	static vec2 parseVec2(String string) {
 		
 		//string.substring(1, string.length()-1);
